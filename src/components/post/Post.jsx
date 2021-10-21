7fbfda9d728feb9b8ec96a46784bbfb8d38bebc6
leroyabiguime <mataa.abiguime17@gmail.com>
2021-10-21 20:27:06 +0000

    Component created: header, post, posts, sidebar, topbar an home page done

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,44 @@
+import './post.css'
+
+const Post = () => {
+    return (
+        <div className="post">
+           <img className="postImg" src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+           />
+           <div className="postInfo">
+               <div className="postCats">
+                    <span className="postCat">Music</span>
+                    <span className="postCat">Life</span>
+               </div>
+               <span className="postTitle">
+               Lorem ipsum dolor sit amet
+               </span>
+               <hr/>
+           <span className="postDate">
+               2 hours ago
+           </span>
+           </div>
+           <p className="postDesc">
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+           Lorem ipsum dolor sit am, consectetur adipiscing el aspect et non
+
+           </p>
+           
+          
+        </div>
+    )
+}
+
+export default Post
