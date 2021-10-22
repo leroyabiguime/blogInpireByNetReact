84cc28511283de02c003d7c88b415a25559d0c40
leroyabiguime <mataa.abiguime17@gmail.com>
2021-10-22 11:46:54 +0000

    up

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/hello', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 
 module.exports = router;
