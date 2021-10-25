4e9ce683dbcbe674d9739637a982ea49c72882ac
leroyabiguime <mataa.abiguime17@gmail.com>
2021-10-25 14:34:56 +0000

    Add getPost Route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -38,7 +38,15 @@ router.put("/:id", async (req, res) => {
     }
   });
 
-
+//GET POST
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //DELETE POST
 router.delete("/:id", async (req, res) => {
     try {
